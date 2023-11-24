80875a6373be4bb8f85bce6d760e54aed26ae27e
Tragiang118 <144584088+Tragiang118@users.noreply.github.com>
2023-11-24 15:23:55 +0700

    Add files via upload

---
+++ Bai1 post 3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class Sum 
+{
+	private:
+    		int A;
+    		int B;
+	public:
+    	Sum(int a, int b) 
+		{
+        	A=a;
+        	B=b;
+    	}
+    	int TinhTong() 
+		{
+			int sum=0;
+			sum=A+B;
+        	return sum;
+    	}
+};
+
+int main() 
+{
+    int A, B;
+    cin>>A>>B;
+    Sum s(A,B);
+    cout<<s.TinhTong();
+    return 0;
+}
